63b958b82adc64d23c78b63acaad01d73b7e044f
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 02:09:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ function destructivelyAppendDriver(name){
 function destructivelyPrependDriver(name){
   drivers.unshift(name);
 }
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  return [...drivers,name]
+}
+function prependDriver(name){
+  return [name,...drivers]
+}
+
+function removeLastDriver(){
+  return drivers.slice(0,drivers.length-1)
+}
+
+function removeFirstDriver(){
+  return drivers.slice(1,drivers.length)
+}
