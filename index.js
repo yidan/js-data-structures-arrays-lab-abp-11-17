e8d0f6e6663041c44171cf738f9b0279c1289df5
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 02:03:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
